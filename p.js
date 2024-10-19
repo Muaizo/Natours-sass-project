c1e69b9bedccccba4c2aad4a093292d7c9dc1498
Abdul Muaiz <muaizbackend@gmail.com>
2024-10-19 12:53:42 +0500

    first commit

---
+++ p.js
@@ -0,0 +1,50 @@
+// // let age = 19; //  block
+// const DOB = 2005; // block.
+// var age = 19; // function based scope.
+
+// const fun = () => {
+//   age = 50;
+// };
+// console.log(age);
+
+// age = 20;
+// age = 21;
+// age = 22;
+// age = 23;
+// let age1 = 20;
+// var age2 = 30;
+// function age() {
+//   if (true) {
+//     var age0 = 10;
+//     console.log('IF statement age value', age0);
+//   }
+//   console.log(age0);
+// }
+// // console.log(age0);
+
+// age();
+
+// stirng
+// symbol
+// number
+// null
+// undefined
+// BigInt
+// Boolean
+// Object
+
+let ab = 90;
+ab = 'afs';
+
+var y = 'gsag';
+y = 9;
+
+// console.log(ab, y);
+
+console.log('5' == 5);
+console.log('5' === 5);
+
+const arr5 = [1, 2, 3, 4, 5];
+
+const [, , , d] = arr5;
+console.log(d);
